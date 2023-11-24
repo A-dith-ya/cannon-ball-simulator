3b7cdcd96dc0c5a79accf3efdb9343cb9e75643d
A-dith-ya <awedithya@gmail.com>
2023-11-24 10:23:07 -0800

    Add rigid body kinematics and dynamics formulas

--- CannonBall.cpp
+++ CannonBall.cpp
@@ -1,11 +1,21 @@
 #include "CannonBall.hpp"
 #include <cmath> 
+#include <iostream>
 
 CannonBall::CannonBall(Cannon& cannon) {
     ball.setRadius(10.f); 
     ball.setPosition(cannon.getBarrelPosition().x, cannon.getBarrelPosition().y);
     ball.setFillColor(sf::Color::Red);
 
+    float velX = initialSpeed * std::cos(cannon.getBarrelOrientation() * (3.14159265 / 180.0));
+    float velY = initialSpeed * std::sin(cannon.getBarrelOrientation() * (3.14159265 / 180.0));
+
+    velocity = sf::Vector2f(velX, velY);
+
+    sf::Vector2f dragForce = -0.5f * dragCoefficient * density * area * velocity;
+    sf::Vector2f totalForce = externalForce + dragForce + mass * gravity;
+    sf::Vector2f acceleration = totalForce / mass;
+    velocity += acceleration;
 }
 
 void CannonBall::draw(sf::RenderWindow& window) {
@@ -13,9 +23,5 @@ void CannonBall::draw(sf::RenderWindow& window) {
 }
 
 void CannonBall::fireCannonBall(Cannon& cannon) {
-    float velX = std::cos(cannon.getBarrelOrientation() * (3.14159265 / 180.0));
-    float velY = std::sin(cannon.getBarrelOrientation() * (3.14159265 / 180.0));
-    
-    velocity = sf::Vector2f(velX, velY);
     ball.move(velocity);
 }
--- CannonBall.hpp
+++ CannonBall.hpp
@@ -12,7 +12,17 @@ public:
 
 private:
     sf::CircleShape ball;
-    sf::Vector2f velocity;
+    sf::Vector2f velocity = sf::Vector2f(1.f, 9.8f);
+    float initialSpeed = 1.f;
+
+    sf::Vector2f externalForce = sf::Vector2f(1.f, -9.8f);
+    
+    float dragCoefficient = 0.5f;
+    float density = 1.0f;
+    float area = 1.0f;
+
+    float mass = 1.0f;
+    sf::Vector2f gravity = sf::Vector2f(0.0f, 9.8f);
 };
 
 #endif
